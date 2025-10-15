1ad76461d9539d24c5dd8bbc2bf84461fb898204
snipeet03 <navneetlonare77@gmail.com>
2025-10-15 05:50:18 +0530

    Add initial Streamlit application with title, text display, dataframe, and line chart

--- 2-Streamlite/app.py
+++ 2-Streamlite/app.py
@@ -5,5 +5,27 @@ import numpy as np
 
 
 ## Title of the application 
-
 st.title("Hello World ")
+
+## Display a simple text there 
+st.write("Hi  there i am navneet there ")
+
+
+## Display the dataframe 
+df = pd.DataFrame({
+    'fistNames' : ['navneet' , 'rahul', 'rajat' ], 
+    'numbers' : [1, 2, 3 ]
+})
+st.write( "Here is my daaframe ")
+st.write(df)
+
+
+# create a line chart 
+chart_data = pd.DataFrame(
+    np.random.randn(20, 3), 
+    columns= ['a', 'b', 'c']
+)
+st.line_chart(chart_data)
+
+
+
